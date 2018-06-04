daa3bd6bb2b486cccead7bb1b02ac2d864f15b1c
haifeiWu <1253242444@qq.com>
2018-06-04 23:46:36 +0800

    update

---
+++ micro-service-dubbo-interface/src/main/java/com/whforever/service/UserService.java
@@ -0,0 +1,4 @@
+package com.whforever.service;
+
+public interface UserService {
+}
---
+++ micro-service-dubbo-provider/src/main/java/com/whforever/service/impl/UserServiceImpl.java
@@ -0,0 +1,8 @@
+package com.whforever.service.impl;
+
+import com.whforever.service.UserService;
+import org.springframework.stereotype.Service;
+
+@Service("userService")
+public class UserServiceImpl implements UserService {
+}
